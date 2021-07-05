1c0993a9b8176a612fb6324af9c00c3dbf83b867
Rjprime <>
2021-07-05 16:04:54 +0000

    Questão 12

--- main.c
+++ main.c
@@ -1,40 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void soma_vetor(int*,int*,int*,int );
-
-int main(void) {
-  int *x, *y,*soma, c,n;
-  printf("Insira quantas componentes terão os vetores a e b:");
-  scanf("%i",&n);
-  x = malloc(n*sizeof(int));
-  if (x == 0 || x == NULL) printf("Erro de alocação");
-
-  y = malloc(n*sizeof(int));
-  if (y == 0 || x == NULL) printf("Erro de alocação");
-  
-  soma = malloc(n*sizeof(int));
-  if (soma == 0 || x == NULL) printf("Erro de alocação");
-
-  printf("\nIndique os valores inteiros p/ a:\n");
-  for (c=0;c<n;c++){
-    scanf("%i",&x[c]);
-  }
-  printf("\nIndique os valores inteiros p/ b:\n");
-  for (c=0;c<n;c++){
-    scanf("%i",&y[c]);
-  }
-  printf("\nO velor soma (a+b) é:(");
-  
-  for (c=0;c<n;c++){
-    if (c == n-1)printf("%i)",soma[c]);
-    else printf("%i,",soma[c]);
-  }
-  return 0;
-}
-void soma_vetor(int*a,int*b,int*soma,int n ){
-  int c;
-  for (c=0;c<n;c++){
-    soma[c] = a[c]+b[c]; 
-  }
+int main (){
+	int a; // a é um inteiro
+	int *ptr;// ptr é um ponteiro p/ armazenar inteiros
+	
+	a = 7;
+	ptr = &// ptr recebe o endereço de a
+	printf("O endereço de a é %p\nO valor de aPtr é %p", &a, aPtr );
+	printf( "\n\nO valor de a é %d\nO valor de *aPtr é %d", a, *aPtr );
+	printf("\n Logo, na linha anterior, usou-se o endereço de a em *ptr p/ acessar o valor lá contido");
+	return 0;
 }
